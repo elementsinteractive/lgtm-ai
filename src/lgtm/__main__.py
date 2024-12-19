c20817f6ce34747c57638178b0e66cb9d12bf1d9
scastlara <s.cast.lara@gmail.com>
2024-12-19 11:24:26 +0100

    feat: allow to customize model

--- src/lgtm/__main__.py
+++ src/lgtm/__main__.py
@@ -1,5 +1,6 @@
 import logging
 from importlib.metadata import version
+from typing import get_args
 
 import click
 import gitlab
@@ -9,6 +10,7 @@ from lgtm.base.schemas import PRUrl
 from lgtm.git_client.gitlab import GitlabClient
 from lgtm.reviewer import CodeReviewer
 from lgtm.validators import parse_pr_url
+from openai.types import ChatModel
 from rich import print
 from rich.logging import RichHandler
 from rich.panel import Panel
@@ -31,19 +33,33 @@ def entry_point() -> None:
 
 @entry_point.command()
 @click.option("--pr-url", required=True, help="The URL of the pull request to review", callback=parse_pr_url)
+@click.option(
+    "--model",
+    type=click.Choice(get_args(ChatModel)),
+    default="gpt-4o-mini",
+    help="The name of the model to use for the review",
+)
 @click.option("--git-api-key", required=True, help="The API key to the git service (GitLab, GitHub, etc.)")
 @click.option("--ai-api-key", required=True, help="The API key to the AI model service (OpenAI, etc.)")
 @click.option("--publish", is_flag=True, help="Publish the review to the git service")
 @click.option("--silent", is_flag=True, help="Do not print the review to the console")
 @click.option("--verbose", "-v", count=True, help="Set logging level")
-def review(pr_url: PRUrl, git_api_key: str, ai_api_key: str, publish: bool, silent: bool, verbose: int) -> None:
+def review(
+    pr_url: PRUrl,
+    model: ChatModel,
+    git_api_key: str,
+    ai_api_key: str,
+    publish: bool,
+    silent: bool,
+    verbose: int,
+) -> None:
     _set_logging_level(logger, verbose)
 
     logger.debug("Parsed PR URL: %s", pr_url)
     logger.info("Starting review of %s", pr_url.full_url)
     git_client = GitlabClient(gitlab.Gitlab(private_token=git_api_key))
     code_reviewer = CodeReviewer(
-        get_basic_agent(api_key=ai_api_key),
+        get_basic_agent(model_name=model, api_key=ai_api_key),
         git_client=git_client,
     )
     review = code_reviewer.review_pull_request(pr_url=pr_url)
