46a6fac0b84cfc395ccccac36e2cff7645ad46c3
Sergio Castillo <s.cast.lara@gmail.com>
2025-08-12 10:45:18 +0200

    feat(#70): support gemini 2.5 stable models (#71)
    closes #70

--- src/lgtm_ai/ai/utils.py
+++ src/lgtm_ai/ai/utils.py
@@ -26,6 +26,16 @@ def select_latest_gemini_model(matches: list[SupportedGeminiModel]) -> Supported
     if len(matches) == 1:
         return matches[0]
 
+    # If one of them is not a `preview` or `exp` model, select it
+    # This is because out of several possible models given a wildcard, we assume preference for stable models.
+    # If the user wanted a preview or experimental model, they could have specified it in the wildcard itself:
+    # e.g.: `gemini-2.5-flash*` will select the latest stable, while `gemini-2.5-flash-p*` will select the latest preview.
+    non_preview_matches = [model for model in matches if all(word not in model for word in ("preview", "exp"))]
+    if non_preview_matches:
+        if len(non_preview_matches) > 1:
+            raise InvalidGeminiWildcard(matches)
+        return non_preview_matches[0]
+
     model_to_date = {model: model.split("-")[-2:] for model in matches}
 
     def _looks_like_date(date: list[str]) -> bool:
