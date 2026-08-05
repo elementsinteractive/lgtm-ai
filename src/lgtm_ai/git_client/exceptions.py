deafae54358252a041360277bf881435a99a0ab3
Sergio Castillo <s.cast.lara@gmail.com>
2026-08-05 12:00:41 +0200

    fix: type errors for new click version

--- src/lgtm_ai/git_client/exceptions.py
+++ src/lgtm_ai/git_client/exceptions.py
@@ -2,49 +2,49 @@ from lgtm_ai.base.exceptions import LGTMException
 
 
 class PullRequestDiffError(LGTMException):
-    message = "Failed to retrieve the diff of the pull request"
+    _message = "Failed to retrieve the diff of the pull request"
 
     def __init__(self) -> None:
-        super().__init__(self.message)
+        super().__init__(self._message)
 
 
 class PullRequestMetadataError(LGTMException):
-    message = "Failed to retrieve the metadata of the pull request"
+    _message = "Failed to retrieve the metadata of the pull request"
 
     def __init__(self) -> None:
-        super().__init__(self.message)
+        super().__init__(self._message)
 
 
 class PullRequestDiffNotFoundError(LGTMException):
-    message = "No diff found for this pull request"
+    _message = "No diff found for this pull request"
 
     def __init__(self) -> None:
-        super().__init__(self.message)
+        super().__init__(self._message)
 
 
 class PublishReviewError(LGTMException):
-    message = "Failed to publish the review"
+    _message = "Failed to publish the review"
 
     def __init__(self) -> None:
-        super().__init__(self.message)
+        super().__init__(self._message)
 
 
 class PublishGuideError(LGTMException):
-    message = "Failed to publish the review guide"
+    _message = "Failed to publish the review guide"
 
     def __init__(self) -> None:
-        super().__init__(self.message)
+        super().__init__(self._message)
 
 
 class InvalidGitAuthError(LGTMException):
-    message = "Invalid Git service authentication token"
+    _message = "Invalid Git service authentication token"
 
     def __init__(self) -> None:
-        super().__init__(self.message)
+        super().__init__(self._message)
 
 
 class DecodingFileError(LGTMException):
-    message = "Failed to decode the file"
+    _message = "Failed to decode the file"
 
     def __init__(self) -> None:
-        super().__init__(self.message)
+        super().__init__(self._message)
